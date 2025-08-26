361e28908de338981f7760bcdab5046c0952e119
Cauê Carneiro <cauecarneiroc@gmail.com>
2025-08-26 17:17:03 -0300

    resolvendo erro do botão na evolutionview

--- FtvApp/Views/Evolution/EvolutionView.swift
+++ FtvApp/Views/Evolution/EvolutionView.swift
@@ -94,7 +94,7 @@ struct EvolutionView: View {
                         HStack(spacing: 4) {
                             Image(systemName: "chevron.left")
                                 .font(.system(size: 16, weight: .medium))
-                            Text("Seus jKKogos")
+                            Text("Seus jogos")
                                 .font(.system(size: 17))
                         }
                         .foregroundColor(Color("ColorPrimal"))
