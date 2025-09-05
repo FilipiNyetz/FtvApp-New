1037a30eae2ff17f1c72d50ef10d38dc9784f9dd
gustavosouto02 <gugasouto2004@gmail.com>
2025-09-05 19:16:25 -0300

    add new logic Views watch

--- FtvApp Watch App/FtvAppApp.swift
+++ FtvApp Watch App/FtvAppApp.swift
@@ -17,7 +17,7 @@ struct FtvApp_Watch_AppApp: App {
     
     var body: some Scene {
         WindowGroup {
-            MainView()
+            ContentView()
         }
     }
 }
--- FtvApp Watch App/Views/StartView.swift
+++ FtvApp Watch App/Views/StartView.swift
@@ -17,7 +17,7 @@ struct StartView: View {
     @State private var savedWorkout: HKWorkout?
     @State private var selectedWorkoutType: HKWorkoutActivityType? = nil
     @StateObject private var jumpDetector = JumpDetector()
-    
+
     @State var numeroWatch: Int = 0
 
     var workoutTypes: [HKWorkoutActivityType] = [.soccer]
@@ -25,26 +25,29 @@ struct StartView: View {
     var body: some View {
         NavigationStack {
             if isWorkoutActive {
-                SessionPagingView(manager: manager, wcSessionDelegate: wcSessionDelegate)
-                    .onAppear {
-                        manager.onWorkoutEnded = { workout in
-                            self.savedWorkout = workout
-                        }
-                        jumpDetector.start()
+                SessionPagingView(
+                    manager: manager,
+                    wcSessionDelegate: wcSessionDelegate
+                )
+                .onAppear {
+                    manager.onWorkoutEnded = { workout in
+                        self.savedWorkout = workout
                     }
-                    .sheet(
-                        item: $savedWorkout,
-                        onDismiss: {
-                            isWorkoutActive = false
-                        }
-                    ) { workout in
-                        SummaryView(
-                            wcSessionDelegate: wcSessionDelegate,
-                            jumpDetector: jumpDetector,
-                            workout: workout
-                        )
-                            .environmentObject(manager)
+                    jumpDetector.start()
+                }
+                .sheet(
+                    item: $savedWorkout,
+                    onDismiss: {
+                        isWorkoutActive = false
                     }
+                ) { workout in
+                    SummaryView(
+                        wcSessionDelegate: wcSessionDelegate,
+                        jumpDetector: jumpDetector,
+                        workout: workout
+                    )
+                    .environmentObject(manager)
+                }
             } else if isCountingDown, let workoutType = selectedWorkoutType {
                 CountdownScreen(onCountdownFinished: {
                     self.isCountingDown = false
@@ -53,70 +56,75 @@ struct StartView: View {
                 })
             } else {
                 //criar a logica de onboarding
-                ZStack{
+                ZStack {
                     Image("LogoS")
                         .resizable()
                         .scaledToFill()
-                        .opacity(0.50)
+                        .opacity(0.20)
                         .ignoresSafeArea()
                         .scaleEffect(0.7)
-                    
-                    LinearGradient(
-                     gradient: Gradient(colors: [.gradiente1, .gradiente2, .gradiente2,  .gradiente2]),
-                            startPoint: .bottomLeading,
-                            endPoint: .topTrailing
-                        )
-                    .opacity(0.85)
-                        .ignoresSafeArea()
-                    
-                    VStack(spacing: 12) {
-                        
-                        HStack{
-                            Button(action: {
-                                numeroWatch -= 1
-                                wcSessionDelegate.sendMessage(message: ["number": numeroWatch])
-                            }, label: {
-                                Text("-")
-                            })
-                            
-                            Text("\(numeroWatch)")
-                            
-                            Button(action: {
-                                numeroWatch += 1
-                                wcSessionDelegate.sendMessage(message: ["number": numeroWatch])
-                            }, label: {
-                                Text("+")
-                            })
-                        }
-                        
-                        //Text("Seu desempenho será registrado em tempo real")
-                        Text("Bem vindo ao SETE, vamos registrar sua performance e evoluir seu jogo")
-                            .font(.title3)
-                            .fontWeight(.medium)
-                            .fontDesign(.rounded)
-                            .multilineTextAlignment(.center)
-                            .foregroundStyle(.white)
-                            .padding(.bottom, 12)
 
+                    LinearGradient(
+                        gradient: Gradient(colors: [
+                            .gradiente1, .gradiente2, .gradiente2, .gradiente2,
+                        ]),
+                        startPoint: .bottomLeading,
+                        endPoint: .topTrailing
+                    )
+                    .opacity(0.85)
+                    .ignoresSafeArea()
+
+                    VStack(spacing: 12) {
+
+                        //Text("Seu desempenho será registrado em tempo real")
+                        Text(
+                            "Seu desempenho será registrado"
+                        )
+                        .font(.title3)
+                        .fontWeight(.medium)
+                        .fontDesign(.rounded)
+                        .multilineTextAlignment(.center)
+                        .foregroundStyle(.white)
 
                         Button(action: {
                             self.selectedWorkoutType = .soccer
                             self.isCountingDown = true
                         }) {
-                            Text("Iniciar")
+                            Text("Iniciar Treino")
                                 .font(.headline)
                                 .fontWeight(.bold)
                                 .foregroundStyle(.black)
                                 .frame(maxWidth: .infinity)
-                                .frame(height: 60)
+                                .frame(width: 180, height: 60)
                                 .background(Color.colorPrimal)
-                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                                .clipShape(RoundedRectangle(cornerRadius: 24))
+                                .padding(.horizontal)
                         }
                         .buttonStyle(.plain)
+
+                        // Botão "Medir salto" - cinza escuro com borda verde
+                        Button(action: {
+                            // Ação vazia conforme solicitado
+                        }) {
+                            Text("Medir salto")
+                                .font(.headline)
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                                .frame(maxWidth: .infinity)
+                                .frame(width: 180, height: 60)
+                                .background(Color.clear)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 24)
+                                        .stroke(Color.colorPrimal, lineWidth: 2)
+                                )
+                                .cornerRadius(24)
+                                .padding(.horizontal)
+                        }
+                        .buttonStyle(PlainButtonStyle())
+
                     }
                     .padding(.horizontal)
                 }
-
 
                 .onAppear {
                     manager.requestAuthorization()
